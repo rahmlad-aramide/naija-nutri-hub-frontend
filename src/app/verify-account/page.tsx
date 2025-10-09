bff3cb972ba4550921066866e7529a4b964a52fe
Abdrahman Oladimeji <abdrahmanoladimeji02@gmail.com>
2025-10-09 17:20:17 +0100

    test: husky setup test ✨✅

--- src/app/verify-account/page.tsx
+++ src/app/verify-account/page.tsx
@@ -1,9 +1,12 @@
 import { VerifyAccountForm } from "@/components/features/signup/verify-account-form";
+import { Suspense } from "react";
 
 export default function VerifyAccountPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <VerifyAccountForm />
+      <Suspense fallback={<p>Loading...</p>}>
+        <VerifyAccountForm />
+      </Suspense>
     </div>
   );
 }
